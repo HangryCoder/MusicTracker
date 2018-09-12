87ab062e49e860b210e23cb54b1b0d7af1b18397
android-genora <android@genora.co.uk>
2018-09-12 15:20:39 +0530

    Commit 1.1 Added Default Swift Networking call. Will start with JSON Serialization.

--- MusicTracker/ViewController.swift
+++ MusicTracker/ViewController.swift
@@ -10,9 +10,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let fetchAudioURL = "http://www.genorainnovations.com/Audio/fetch_audio.php"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let config = URLSessionConfiguration.default
+        let session = URLSession(configuration: config)
+        let url = URL(string: fetchAudioURL)!
+        let task = session.dataTask(with: url) { (data, response, error) in
+            if error != nil {
+                print(error!.localizedDescription)
+            } else {
+                do{
+                    let jsonResponse = try JSONSerialization.jsonObject(with:
+                        data!, options: [])
+                    print(jsonResponse) //Response result
+                } catch let parsingError {
+                    print("Error", parsingError)
+                }
+            }
+        }
+        task.resume()
     }
 
 }
