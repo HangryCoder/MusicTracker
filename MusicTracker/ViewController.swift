ee606a73586cb58cba3d38ecca943f387a06f48c
android-genora <android@genora.co.uk>
2018-09-12 13:16:09 +0530

    Commit 1.0 First Commit

---
+++ MusicTracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MusicTracker
+//
+//  Created by Sonia Wadji on 12/09/18.
+//  Copyright © 2018 genora. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+}
+
